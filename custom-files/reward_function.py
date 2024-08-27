280c5812af268ad7e7ccf20f42dbe09a7e32bdd9
foreignkey <florkaucic@gmail.com>
2024-08-26 23:14:17 -0300

    Changing comment

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -233,17 +233,18 @@ def reward_function(params):
 
     reward *= (1.0 - abs((optimal_speed - speed) / optimal_speed))  # affect reward based on speed
 
-    # uncomment if not fast enough (tweak constants at the beginning)
+    # uncomment if not fast enough (tweak constants at the beginning, and it should probably be proportional)
     # if (
     #     prev_point > 103
     #     or next_point < 12
     # ):
-    #     if speed < MIN_SPEED_ON_STRAIGHT_PATH:
-    #         # Heavily penalize reward if the car doesn't go flat out on straight paths
-    #         reward *= 0.5
     #     if abs_steering > ABS_STEERING_ON_STRAIGHT_PATH_THRESHOLD:
     #         # Penalize reward if the car is steering too much on straight paths
     #         reward *= 0.6
+    #
+    #     if speed < MIN_SPEED_ON_STRAIGHT_PATH:
+    #         # Heavily penalize reward if the car doesn't go flat out on straight paths
+    #         reward *= 0.5
 
     reward = float(reward)
 
